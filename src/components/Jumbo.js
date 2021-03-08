6b57af304193c2194c38a1505e77ac1c0f53d825
Arnie Alano <arniealano@gmail.com>
2021-03-08 19:36:10 +0800

    updated component jumbo

--- src/components/Jumbo.js
+++ src/components/Jumbo.js
@@ -6,9 +6,9 @@ import Button from 'react-bootstrap/Button'
 export default function Jumbo() {
     return (
         <Jumbotron>
-            <h1>Hello, world!</h1>
-            <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-            <Button variant="primary">Learn more</Button>
+            <h1>Batch 101 - Hello user!</h1>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <Button variant="primary">Go to Blogs</Button>
         </Jumbotron>
     )
 }
